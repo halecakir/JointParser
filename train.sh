10d7b1134791d0b82b29326e34019d749d9834a1
Huseyin <huseyinalecakir@gmail.com>
2019-02-14 19:36:33 +0300

    Bash script is completed

--- train.sh
+++ train.sh
@@ -1,4 +1,66 @@
-python jPTDP.py --dynet-seed  123456789 \
+TYPE={$1-None}
+UDTYPE=$2
+
+UDTRAIN=""
+UDTEST=""
+
+if [ $UDTYPE = "2.2" ]; then
+    echo "UD_TYPE is 2.2"
+    UDTRAIN="/home/huseyin/Data/ud-treebanks-v2.2/UD_Turkish-IMS/tr_imst-ud-train.conllu"
+    UDTEST="/home/huseyin/Data/ud-treebanks-v2.2/UD_Turkish-IMS/tr_imst-ud-test.conllu"
+else
+    echo "UD_TYPE is 2.3"
+    UDTRAIN="/home/huseyin/Data/ud-treebanks-v2.3/UD_Turkish-IMS/tr_imst-ud-train.conllu"
+    UDTEST="/home/huseyin/Data/ud-treebanks-v2.3/UD_Turkish-IMS/tr_imst-ud-test.conllu"
+fi
+
+if [ $TYPE = "seg" ]; then
+    echo "Only Segmentation"
+    python jPTDP.py --dynet-seed  123456789 \
+            --dynet-mem 1000 \
+            --epochs 12 \
+            --lstmdims 128 \
+            --lstmlayers 2 \
+            --hidden 100 \
+            --wembedding 200 \
+            --cembedding 50 \
+            --membedding 50 \
+            --tembedding 50 \
+            --pembedding 100 \
+            --model "$TYPE-trialmodel" \
+            --params "$TYPE-trialmodel.params" \
+            --outdir /home/huseyin/outdir \
+            --train $UDTRAIN \
+            --dev $UDTEST \
+            --segmentation /home/huseyin/Data/metu.tr \
+            --disablemorphtag \
+            --disablepipeline \
+            --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+elif [ $TYPE = "morphTag" ]; then
+    echo "Only Morph Tagging"
+    python jPTDP.py --dynet-seed  123456789 \
+            --dynet-mem 1000 \
+            --epochs 12 \
+            --lstmdims 128 \
+            --lstmlayers 2 \
+            --hidden 100 \
+            --wembedding 200 \
+            --cembedding 50 \
+            --membedding 50 \
+            --tembedding 50 \
+            --pembedding 100 \
+            --model "$TYPE-trialmodel" \
+            --params "$TYPE-trialmodel.params" \
+            --outdir /home/huseyin/outdir \
+            --train $UDTRAIN \
+            --dev $UDTEST \
+            --segmentation /home/huseyin/Data/metu.tr \
+            --disablemorph \
+            --disablepipeline \
+            --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+elif [ $TYPE = "jointAll" ]; then
+    echo "Joint All Tasks"
+    python jPTDP.py --dynet-seed  123456789 \
                 --dynet-mem 1000 \
                 --epochs 12 \
                 --lstmdims 128 \
@@ -9,37 +71,35 @@ python jPTDP.py --dynet-seed  123456789 \
                 --membedding 50 \
                 --tembedding 50 \
                 --pembedding 100 \
-                --model trialmodel \
-                --params trialmodel.params \
-                --outdir /home/huseyin/Desktop/NLP/outdir \
-                --train /home/huseyin/Desktop/NLP/data/UD_Turkish-IMST/tr_imst-ud-train.conllu \
-                --dev /home/huseyin/Desktop/NLP/data/UD_Turkish-IMST/tr_imst-ud-dev.conllu \
-                --segmentation /home/huseyin/Desktop/NLP/data/metu.tr \
-                --disablemorph \
-                --disablepipeline \
-                --disablemorphtag \
-                --prevectors /home/huseyin/Desktop/NLP/data/Turkish/tr.vectors.xz
-                #--prevectors /home/huseyin/Desktop/NLP/data/boun_full_vector 
-
-
-python jPTDP.py --dynet-seed  123456789 \
-                --dynet-mem 1000 \
-                --epochs 12 \
-                --lstmdims 128 \
-                --lstmlayers 2 \
-                --hidden 100 \
-                --wembedding 200 \
-                --cembedding 50 \
-                --membedding 50 \
-                --tembedding 50 \
-                --pembedding 100 \
-                --model trialmodel \
-                --params trialmodel.params \
+                --model "$TYPE-trialmodel" \
+                --params "$TYPE-trialmodel.params" \
                 --outdir /home/huseyin/outdir \
-                --train /home/huseyin/Data/ud-treebanks-v2.2/UD_Turkish-IMST/tr_imst-ud-train.conllu \
-                --dev /home/huseyin/Data/ud-treebanks-v2.2/UD_Turkish-IMST/tr_imst-ud-test.conllu \
+                --train $UDTRAIN \
+                --dev $UDTEST \
+                --segmentation /home/huseyin/Data/metu.tr \
+                --disablepipeline \
+                --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+else
+    echo "Base Dependency Model"
+    python jPTDP.py --dynet-seed  123456789 \
+                --dynet-mem 1000 \
+                --epochs 12 \
+                --lstmdims 128 \
+                --lstmlayers 2 \
+                --hidden 100 \
+                --wembedding 200 \
+                --cembedding 50 \
+                --membedding 50 \
+                --tembedding 50 \
+                --pembedding 100 \
+                --model "$TYPE-trialmodel" \
+                --params "$TYPE-trialmodel.params" \
+                --outdir /home/huseyin/outdir \
+                --train $UDTRAIN \
+                --dev $UDTEST \
                 --segmentation /home/huseyin/Data/metu.tr \
                 --disablemorph \
                 --disablepipeline \
                 --disablemorphtag \
                 --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+fi
