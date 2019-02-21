eabe07cd949e2a975fe2c3d968cc7370b4962034
Huseyin Alecakir <huseyinalecakir@gmail.com>
2019-02-21 22:45:05 +0200

    Environmet variable is added

--- train.sh
+++ train.sh
@@ -1,4 +1,23 @@
-TYPE={$1-None}
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo 'Usage : sh train.sh EXPERIMENT_TYPE UDTYPE'
+    echo 'Example: sh train.sh jointAll 2.2'
+    exit 0
+fi
+
+if [[ -z "${DATASET}" ]]; then
+  echo "DATASET environment variable is undefined"
+else
+  DATASET_VARIABLE="${DATASET}"
+  echo "DATASET located in ${DATASET}"
+  
+fi
+
+#Create outdir if not exist
+mkdir -p ../outdir
+
+TYPE=$1
 UDTYPE=$2
 
 UDTRAIN=""
@@ -6,12 +25,15 @@ UDTEST=""
 
 if [ $UDTYPE = "2.2" ]; then
     echo "UD_TYPE is 2.2"
-    UDTRAIN="/home/huseyin/Data/ud-treebanks-v2.2/UD_Turkish-IMS/tr_imst-ud-train.conllu"
-    UDTEST="/home/huseyin/Data/ud-treebanks-v2.2/UD_Turkish-IMS/tr_imst-ud-test.conllu"
-else
+    UDTRAIN="$DATASET_VARIABLE/ud-treebanks-v2.2/UD_Turkish-IMST/tr_imst-ud-train.conllu"
+    UDTEST="$DATASET_VARIABLE/ud-treebanks-v2.2/UD_Turkish-IMST/tr_imst-ud-test.conllu"
+elif [ $UDTYPE = "2.3" ]; then
     echo "UD_TYPE is 2.3"
-    UDTRAIN="/home/huseyin/Data/ud-treebanks-v2.3/UD_Turkish-IMS/tr_imst-ud-train.conllu"
-    UDTEST="/home/huseyin/Data/ud-treebanks-v2.3/UD_Turkish-IMS/tr_imst-ud-test.conllu"
+    UDTRAIN="$DATASET_VARIABLE/ud-treebanks-v2.3/UD_Turkish-IMST/tr_imst-ud-train.conllu"
+    UDTEST="$DATASET_VARIABLE/ud-treebanks-v2.3/UD_Turkish-IMST/tr_imst-ud-test.conllu"
+else
+    echo "Invalid UD_TYPE"
+    echo "Possible UD types: 2.2 and 2.3"
 fi
 
 if [ $TYPE = "seg" ]; then
@@ -22,20 +44,20 @@ if [ $TYPE = "seg" ]; then
             --lstmdims 128 \
             --lstmlayers 2 \
             --hidden 100 \
-            --wembedding 200 \
+            --wembedding 100 \
             --cembedding 50 \
             --membedding 50 \
             --tembedding 50 \
             --pembedding 100 \
             --model "$TYPE-trialmodel" \
             --params "$TYPE-trialmodel.params" \
-            --outdir /home/huseyin/outdir \
+            --outdir ../outdir \
             --train $UDTRAIN \
             --dev $UDTEST \
-            --segmentation /home/huseyin/Data/metu.tr \
+            --segmentation $DATASET_VARIABLE/metu.tr \
             --disablemorphtag \
             --disablepipeline \
-            --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+            --prevectors $DATASET_VARIABLE/Turkish/tr.vectors.xz
 elif [ $TYPE = "morphTag" ]; then
     echo "Only Morph Tagging"
     python jPTDP.py --dynet-seed  123456789 \
@@ -44,20 +66,20 @@ elif [ $TYPE = "morphTag" ]; then
             --lstmdims 128 \
             --lstmlayers 2 \
             --hidden 100 \
-            --wembedding 200 \
+            --wembedding 100 \
             --cembedding 50 \
             --membedding 50 \
             --tembedding 50 \
             --pembedding 100 \
             --model "$TYPE-trialmodel" \
             --params "$TYPE-trialmodel.params" \
-            --outdir /home/huseyin/outdir \
+            --outdir ../outdir \
             --train $UDTRAIN \
             --dev $UDTEST \
-            --segmentation /home/huseyin/Data/metu.tr \
+            --segmentation $DATASET_VARIABLE/metu.tr \
             --disablemorph \
             --disablepipeline \
-            --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+            --prevectors $DATASET_VARIABLE/Turkish/tr.vectors.xz
 elif [ $TYPE = "jointAll" ]; then
     echo "Joint All Tasks"
     python jPTDP.py --dynet-seed  123456789 \
@@ -66,20 +88,20 @@ elif [ $TYPE = "jointAll" ]; then
                 --lstmdims 128 \
                 --lstmlayers 2 \
                 --hidden 100 \
-                --wembedding 200 \
+                --wembedding 100 \
                 --cembedding 50 \
                 --membedding 50 \
                 --tembedding 50 \
                 --pembedding 100 \
                 --model "$TYPE-trialmodel" \
                 --params "$TYPE-trialmodel.params" \
-                --outdir /home/huseyin/outdir \
+                --outdir ../outdir \
                 --train $UDTRAIN \
                 --dev $UDTEST \
-                --segmentation /home/huseyin/Data/metu.tr \
+                --segmentation $DATASET_VARIABLE/metu.tr \
                 --disablepipeline \
-                --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
-else
+                --prevectors $DATASET_VARIABLE/Turkish/tr.vectors.xz
+elif [ $TYPE = "base" ]; then
     echo "Base Dependency Model"
     python jPTDP.py --dynet-seed  123456789 \
                 --dynet-mem 1000 \
@@ -87,19 +109,23 @@ else
                 --lstmdims 128 \
                 --lstmlayers 2 \
                 --hidden 100 \
-                --wembedding 200 \
+                --wembedding 100 \
                 --cembedding 50 \
                 --membedding 50 \
                 --tembedding 50 \
                 --pembedding 100 \
                 --model "$TYPE-trialmodel" \
                 --params "$TYPE-trialmodel.params" \
-                --outdir /home/huseyin/outdir \
+                --outdir ../outdir \
                 --train $UDTRAIN \
                 --dev $UDTEST \
-                --segmentation /home/huseyin/Data/metu.tr \
+                --segmentation $DATASET_VARIABLE/metu.tr \
                 --disablemorph \
                 --disablepipeline \
                 --disablemorphtag \
-                --prevectors /home/huseyin/Data/Turkish/tr.vectors.xz
+                --prevectors $DATASET_VARIABLE/Turkish/tr.vectors.xz
+else
+   echo "Invalid Experiment Type"
+   echo "Valid types: seg, morphTag, jointAll, and base" 
 fi
+
