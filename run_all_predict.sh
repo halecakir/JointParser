ef399e8f0c4c22c53a2aa3acac2020a1c38eca70
Huseyin Alecakir <huseyinalecakir@gmail.com>
2020-01-18 21:38:31 +0300

    Commands updated

--- run_all_predict.sh
+++ run_all_predict.sh
@@ -1,4 +1,4 @@
-sh train.sh base  2.2 predict Turkish
-sh train.sh seg  2.2 predict Turkish
-sh train.sh morphTag  2.2 predict Turkish
-sh train.sh jointAll  2.2 predict Turkish
+sh train.sh base  2.2 predict Turkish 1
+sh train.sh seg  2.2 predict Turkish 1
+sh train.sh morphTag  2.2 predict Turkish 1
+sh train.sh jointAll  2.2 predict Turkish 1
