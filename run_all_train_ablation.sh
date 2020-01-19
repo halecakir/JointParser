a95c8feaada630a30047111e892147c466314266
Huseyin Alecakir <huseyinalecakir@gmail.com>
2020-01-19 14:41:58 +0200

    Pipelined ablation is done. In this version, I just replaced the predicted tags with the gold ones in the prediction step.

--- run_all_train_ablation.sh
+++ run_all_train_ablation.sh
@@ -1,5 +1,3 @@
-sh train.sh segAblation  2.2 train Turkish 1
-sh train.sh morphTagAblation  2.2 train Turkish 1
-sh train.sh jointAllAblationSeg  2.2 train Turkish 1
-sh train.sh jointAllAblationMorphTag  2.2 train Turkish 1
-sh train.sh jointAllAblationBoth  2.2 train Turkish 1
+sh train.sh jointAllAblationSeg  2.2 train Turkish 15
+sh train.sh jointAllAblationMorphTag  2.2 train Turkish 15
+sh train.sh jointAllAblationBoth  2.2 train Turkish 15
