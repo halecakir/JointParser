a557f9dedcd0ac9eb7d47026da93e35a38ed950c
Huseyin Alecakir <huseyinalecakir@gmail.com>
2020-04-01 16:32:08 +0300

    Redundant files are removed

--- run_all_predict_ablation.sh
+++
@@ -1,3 +0,0 @@
-sh train.sh jointAllAblationSeg  2.2 predict Turkish 1
-sh train.sh jointAllAblationMorphTag  2.2 predict Turkish 1
-sh train.sh jointAllAblationBoth  2.2 predict Turkish 1
--- run_all_train_ablation.sh
+++
@@ -1,3 +0,0 @@
-sh train.sh jointAllAblationSeg  2.2 train Turkish 15
-sh train.sh jointAllAblationMorphTag  2.2 train Turkish 15
-sh train.sh jointAllAblationBoth  2.2 train Turkish 15
